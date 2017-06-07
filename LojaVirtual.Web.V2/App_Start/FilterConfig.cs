6aee3f70b3a87b203d5948ce1b98194dcb85abe0
Juninho Castro <junin_castro@hotmail.com>
2017-06-07 14:25:09 -0300

    Adicionar arquivos de projeto.

---
+++ LojaVirtual.Web.V2/App_Start/FilterConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using LojaVirtual.Web.V2.HtmlHelpers;
+
+namespace LojaVirtual.Web.V2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+
+            filters.Add(new InjectPageMetadataAttribute());
+        }
+    }
+}
---
+++ LojaVirtual.Web.V2/Models/CategoriaSubGruposViewModel.cs
@@ -0,0 +1,12 @@
+﻿using LojaVirtual.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace LojaVirtual.Web.V2.Models
+{
+    public class CategoriaSubGruposViewModel
+    {
+        public Categoria Categoria { get; set; }
+
+        public IEnumerable<SubGrupo> SubGrupos { get; set; }
+    }
+}
